b8f46ec9c8d6c52b25f17028e8fe808141f0218b
Anatoly <boronilov@gmail.com>
2023-05-06 12:54:37 +0200

    project init

--- notification/celery.py
+++ notification/celery.py
@@ -1,1 +1,8 @@
 from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notification.settings")
+app = Celery("notification")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
