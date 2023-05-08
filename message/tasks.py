39e8cdfe9aed81e40ecf6419e8eb52979251fba1
Anatoly <boronilov@gmail.com>
2023-05-08 22:21:23 +0200

    logger

--- message/tasks.py
+++ message/tasks.py
@@ -1,9 +1,11 @@
 from notification.celery import app
+from celery.utils.log import get_task_logger
 from .models import Message
 from django.conf import settings
 import requests 
 import json
 
+logger = get_task_logger(__name__)
 
 @app.task(name="send.message", bind=True, max_retries=10)
 def send_message(self, message_id, client_id, text):
@@ -21,12 +23,12 @@ def send_message(self, message_id, client_id, text):
     try:
         response = requests.post(url=url, headers=headers, data=json.dumps(data))
     except Exception:
-        # logger
+        logger.error(f"Message with id {message_id} is not sent due to server error on sender side, retrying...")
         self.retry(coundtdown=5, **self.request.retries)
     else:
         message = Message.objects.get(pk=data["id"])
         message.is_send = True
         message.save()
-        # logger
+        logger.info(f"Message with id {message_id} is successfully sent")
     
     return('sender', response.status_code)
